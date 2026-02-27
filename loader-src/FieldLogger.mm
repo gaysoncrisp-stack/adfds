7d10ceb4afe148b3a2cb752e06d47257ebf60fbc
Crisp <245650982+gaysoncrisp-stack@users.noreply.github.com>
2026-02-26 21:24:54 -0700

    test

--- loader-src/FieldLogger.mm
+++ loader-src/FieldLogger.mm
@@ -5021,15 +5021,15 @@ void initStuff(MemoryFileInfo framework)
             sleep(1);
         }
        std::thread([] {
-                sleep(10);
+        @autoreleasepool 
+        {
+            sleep(10);
                auto m_SendDestroyOfAll = s_get_method_from_name(PhotonNetwork, "SendDestroyOfAll", 0);
                s_runtime_invoke(m_SendDestroyOfAll, nullptr, nullptr, &ex);
                NSLog(@"[Kitty] destroyed all...");
-               
-        }).detach();
-
-
-
+        }
+    }).detach();
+    
     StartConfigPoll();
     StartFramePump();
 }
